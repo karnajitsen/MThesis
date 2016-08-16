ed897b666c4604ed60276e7af5e1ded744aa242f
karnajit sen <karnajitsen@gmail.com>
2016-08-16 15:31:57 +0200

    add

---
+++ exp1/execute.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+#include <time.h>
+int main()
+{
+
+   FILE *fp;
+   int m, stride; //, n, p, pr = 4, csize = 8;
+   //struct timeval tv1, tv2;
+   m = 0;
+   // gettimeofday(&tv2, NULL);
+   fp = fopen("execute.sh", "w+");
+   fprintf(fp, "cd data\n");
+   fprintf(fp, "mv result.txt result.bk_%d\n",(int)time(NULL));
+   
+   fprintf(fp, "cd ..\n");
+
+   while ( (m*8.0/1024 ) < 4000)
+        {
+   fprintf(fp, "./kr %d %d\n", m, stride);
+    m +=256;
+    }
+   //fprintf(fp, "gnuplot plot.in\n");
+   fclose(fp);
+   return 0;
+}
